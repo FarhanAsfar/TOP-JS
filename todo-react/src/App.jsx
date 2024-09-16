1409d9366f2943d3db6e67bdf6c035875a35ae3f
arnob <farhanasfar21@gmail.com>
2024-09-17 02:52:23 +0600

    Iterate through an array and render it

--- todo-react/src/App.jsx
+++ todo-react/src/App.jsx
@@ -4,32 +4,34 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todo, setTodo] = useState([{
+    title: "TOP",
+    description: "Full Stack",
+    status: false
+  },  {
+    title: "Web3",
+    description: "Blockchain",
+    status: false
+  }])
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      {/* <Todo title="TOP" description="Full Stack"></Todo> */}
+
+      {
+        todo.map(function(todo){
+          return <Todo title={todo.title} description={todo.description}></Todo>
+        })
+      }
+    </div>
   )
+}
+
+function Todo(props){
+  return <div>
+    <h1>{props.title}</h1>
+    <h2>{props.description}</h2>
+  </div>
 }
 
 export default App
