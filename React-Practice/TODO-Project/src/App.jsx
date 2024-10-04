4bda284d59137b785b3e8bd74afaa4a4dcd38071
arnob <farhanasfar21@gmail.com>
2024-10-04 17:54:16 +0600

    Define 'AddTodo' component

--- React-Practice/TODO-Project/src/App.jsx
+++ React-Practice/TODO-Project/src/App.jsx
@@ -1,27 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import AppName from './components/AppName'
+import AddTodo from './components/AddTodo'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <center class = 'todo-container'>
-      <h1>TODO App</h1>
-      <div class="container text-center">
-        <div class="row">
-          <div class="col-6">
-            <input type="text" placeholder='Enter Todo' />
-          </div>
-          <div class="col-4">
-            <input type="date" />
-          </div>
-          <div class="col-2">
-            <button type='button' class="btn btn-success">Add</button>
-          </div>
-        </div>
-      </div>
+      <AppName />
+      <AddTodo />
     </center>
   )
 }
