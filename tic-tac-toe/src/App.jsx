ec5691aeb95a1cab8e734ae5f18ff993cc52ca08
arnob <farhanasfar21@gmail.com>
2024-10-08 03:28:11 +0600

    Create game board

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -2,12 +2,27 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div>
-      <button className='square'>X</button>
-    </div>
+    <>
+      <div className='board-row'>
+        <button className='square'>X</button>
+        <button className='square'>X</button>
+        <button className='square'>X</button>
+      </div>
+
+      <div className='board-row'>
+        <button className='square'>X</button>
+        <button className='square'>X</button>
+        <button className='square'>X</button>
+      </div>
+
+      <div className='board-row'>
+        <button className='square'>X</button>
+        <button className='square'>X</button>
+        <button className='square'>X</button>
+      </div>
+    </>
   )
 }
 
