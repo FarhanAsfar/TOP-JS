6cab10ff1f2b75e8ff131689bd8725af9097d0a7
arnob <farhanasfar21@gmail.com>
2024-10-08 03:30:44 +0600

    Move 'Board' to components folder

---
+++ tic-tac-toe/src/components/board.jsx
@@ -0,0 +1,23 @@
+function Board(){
+    <div>
+        <div className='board-row'>
+        <button className='square'>X</button>
+        <button className='square'>X</button>
+        <button className='square'>X</button>
+      </div>
+
+      <div className='board-row'>
+        <button className='square'>X</button>
+        <button className='square'>X</button>
+        <button className='square'>X</button>
+      </div>
+
+      <div className='board-row'>
+        <button className='square'>X</button>
+        <button className='square'>X</button>
+        <button className='square'>X</button>
+      </div>
+    </div>
+}
+
+export default Board;
