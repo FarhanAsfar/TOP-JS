b1778fed0238b65ab55c2c174498bd953184f0df
arnob <farhanasfar21@gmail.com>
2024-10-14 03:27:27 +0600

    Display text

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -29,6 +29,14 @@ export default function Board() {
     setXIsNext(!xIsNext);
   }
 
+  const winner = calculateWinner(squares);
+  let status;
+  if(winner){
+    status = "Winner: "+winner;
+  }else{
+    status = "Looser: "+ (xIsNext ? "X" : "O");
+  }
+
   return (
     <div>
       <div className='board-row'>
