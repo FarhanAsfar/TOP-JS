211bc7a70ea3acb5bd050f963dd7ffbeefcdf917
arnob <farhanasfar21@gmail.com>
2024-10-19 08:17:44 +0600

    Conditional Rendering

--- React-Practice-KG/Fragments/src/App.jsx
+++ React-Practice-KG/Fragments/src/App.jsx
@@ -1,13 +1,15 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-
+  // const fruits = [];
   const fruits = ["Apple", "Banana", "Grape", "Cherry", "Berry"];
 
   return (
     <>
       <h2>No extra div, using fragment</h2>
+
       <h1>Fruit List</h1>
+      {fruits.length===0 ? <h3>Empty List</h3>:null}
 
       <ul className="list-group">
         {fruits.map(item => (
