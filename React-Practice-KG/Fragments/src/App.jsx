425ac250c15aea1551cea431bb13ea16a85b7e88
arnob <farhanasfar21@gmail.com>
2024-10-19 08:06:28 +0600

    Add bootstrap

--- React-Practice-KG/Fragments/src/App.jsx
+++ React-Practice-KG/Fragments/src/App.jsx
@@ -1,11 +1,19 @@
-import { useState } from 'react'
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const fruits = ["Apple", "Banana", "Grape", "Cherry", "Berry"];
 
   return (
     <>
       <h2>No extra div, using fragment</h2>
+      <h1>Fruit List</h1>
+
+      <ul class="list-group">
+        {fruits.map(item => (
+          <li class="list-group-item">{item}</li>
+        ))}
+      </ul>
     </>
   )
 }
