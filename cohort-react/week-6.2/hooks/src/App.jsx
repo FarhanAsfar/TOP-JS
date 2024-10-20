1dde7559f5423018592f414825b10cb90e8074e1
arnob <farhanasfar21@gmail.com>
2024-10-21 00:10:13 +0600

    Fetch todo list

--- cohort-react/week-6.2/hooks/src/App.jsx
+++ cohort-react/week-6.2/hooks/src/App.jsx
@@ -1,14 +1,29 @@
+import { useEffect } from 'react'
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState(0)
+
+  useEffect(() => {
+    fetch("http://localhost:3000")
+    .then(async (res) => {
+      const json = await res.json();
+      setTodos(json.data);
+    });
+  }, []);
 
   return (
     <div>
-      
+      {todos.map(todo => <Todo key={todo._id} title={todo.title} description={todo.description} />)}
+    </div>
+  )
+}
+
+function Todo({title, description}){
+  return(
+    <div>
+      <h1>{title}</h1>
+      <h3>{description}</h3>
     </div>
   )
 }
