3ddd608faae8ae02a197a4796eb99a6e034061d7
arnob <farhanasfar21@gmail.com>
2024-10-21 01:55:56 +0600

    Fetch specific todo with id and render it

--- cohort-react/week-6.2/hooks/src/App.jsx
+++ cohort-react/week-6.2/hooks/src/App.jsx
@@ -2,34 +2,41 @@ import { useEffect } from 'react'
 import { useState } from 'react'
 
 function App() {
-  const [todos, setTodos] = useState([]);
-
-  useEffect(() => {
-    try{
-      fetch("http://localhost:3000/todos")
-      .then(async (res) => {
-        const json = await res.json();
-        //console.log(json);
-        setTodos(json.data);
-      })
-    }
-    catch(error){
-      console.log(error);
-    }
-  }, []);
-
+  
   return (
     <div>
-      {todos.map(todo => <Todo key={todo._id} title={todo.title} description={todo.description} />)}
+      <Todo id="66ebadc0740e2c7586675c8c"></Todo>
     </div>
   );
 }
 
-function Todo({title, description}){
+function Todo({id}){
+  const [todos, setTodos] = useState(null);
+
+  useEffect(() => {
+    const fetchTodo = async () => {
+      try{
+        const res =await fetch(`http://localhost:3000/todos/${id}`);
+        const json = await res.json();
+        setTodos(json);
+      }
+      catch(error){
+        console.log(error);
+      }
+    };
+
+    fetchTodo();
+  }, [id]);
+
+  if (!todos) {
+    return <div>Loading...</div>;
+  }
+
+  
   return(
     <div>
-      <h1>{title}</h1>
-      <h3>{description}</h3>
+      <h1>{todos.title}</h1>
+      <h3>{todos.description}</h3>
     </div>
   );
 }
