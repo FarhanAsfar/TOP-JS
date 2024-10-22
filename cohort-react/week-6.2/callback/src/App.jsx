974b3257c98d3855d56bc4884b8ec67437fff90c
arnob <farhanasfar21@gmail.com>
2024-10-22 10:45:13 +0600

    useCallback

--- cohort-react/week-6.2/callback/src/App.jsx
+++ cohort-react/week-6.2/callback/src/App.jsx
@@ -1,14 +1,18 @@
+import { useCallback } from 'react';
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  const inputFunction = useCallback(() => {
+    console.log("inside function");
+  }, []);
+  
   return (
     <div>
-      
+      <button onClick={() => {
+        setCount(count+1);
+      }}>Count ({count})</button>      
     </div>
   )
 }
