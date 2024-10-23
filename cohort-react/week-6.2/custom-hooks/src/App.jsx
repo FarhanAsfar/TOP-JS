dcd7ee2739f1f6f85abd6857e85c2006825b359a
arnob <farhanasfar21@gmail.com>
2024-10-24 02:30:38 +0600

    custom hooks

--- cohort-react/week-6.2/custom-hooks/src/App.jsx
+++ cohort-react/week-6.2/custom-hooks/src/App.jsx
@@ -1,11 +1,32 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+
+//custom hook
+function useTodos(){
+  const [todos, setTodos] = useState(null);
+
+  useEffect(() => {
+    const fetchTodo = async () => {
+      try{
+        const res =await fetch(`http://localhost:3000/todos/`);
+        const json = await res.json();
+        setTodos(json);
+      }
+      catch(error){
+        console.log(error);
+      }
+    };
+
+    fetchTodo();
+  }, []);
+}
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const todos = useTodos();
 
   return (
     <div>
-      
+      {todos}    
     </div>
   )
 }
