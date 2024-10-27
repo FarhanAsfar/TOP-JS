42d14150553f272a6d3f99d7ab271cb08fd3e4ce
arnob <farhanasfar21@gmail.com>
2024-10-28 03:02:20 +0600

    Complete use-callback assignment1

--- cohort-react/week6-assignment/2-use-callback/src/components/components/Assignment1.jsx
+++ cohort-react/week6-assignment/2-use-callback/src/components/components/Assignment1.jsx
@@ -1,18 +1,22 @@
-import { useState } from "react";
-
+import React, { useCallback, useState } from "react";
+import { memo } from "react";
 // Create a counter component with increment and decrement functions. Pass these functions to a child component which has buttons to perform the increment and decrement actions. Use useCallback to ensure that these functions are not recreated on every render.
 
 export function Assignment1() {
     const [count, setCount] = useState(0);
 
     // Your code starts here
-    function handleIncrement() {
-        setCount(count+1);
-    }
+    const  handleIncrement = useCallback(() => {
+        setCount(function(currentCount){
+            return currentCount+1;
+        });
+    }, []);
 
-    function handleDecrement() {
-        setCount(count-1);
-    }
+    const handleDecrement = useCallback(() => {
+        setCount(function(currentCount){
+            return currentCount-1;
+        });
+    }, []);
     // Your code ends here
 
     return (
@@ -23,9 +27,9 @@ export function Assignment1() {
     );
 };
 
-const CounterButtons = ({ onIncrement, onDecrement }) => (
+const CounterButtons = memo(({onIncrement, onDecrement}) => (
     <div>
         <button onClick={onIncrement}>Increment</button>
         <button onClick={onDecrement}>Decrement</button>
     </div>
-);
+));
