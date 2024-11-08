6d4f69c0a6f363ed74feede2ba41b8b34037c6d0
arnob <farhanasfar21@gmail.com>
2024-11-08 11:31:00 +0600

    Atom family

---
+++ cohort-react/week7/atom-family/src/App.jsx
@@ -0,0 +1,25 @@
+import { RecoilRoot } from 'recoil';
+
+function App() {
+
+  return (
+    <>
+      <RecoilRoot>
+        <Todo id={1} />
+        <Todo id={2} />
+      </RecoilRoot>
+    </>
+  )
+}
+
+function Todo({id}){
+  return(
+    <>
+      {todo.title}
+      {todo.description}
+      <br />
+    </>
+  )
+}
+
+export default App
---
+++ cohort-react/week7/atom-family/src/todos.js
@@ -0,0 +1,10 @@
+export const TODOS = [{
+    id:1,
+    title: 'MERN',
+    description: 'web dev'
+},
+{
+    id:2,
+    title: 'WEB3',
+    description: 'Blockchain'
+}]
