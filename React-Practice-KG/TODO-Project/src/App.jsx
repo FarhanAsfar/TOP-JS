21554fcb8d9c3e0523ea9eb8fa4701406a4e82a6
arnob <farhanasfar21@gmail.com>
2024-11-21 18:32:55 +0600

    change

--- React-Practice-KG/TODO-Project/src/App.jsx
+++ React-Practice-KG/TODO-Project/src/App.jsx
@@ -1,10 +1,8 @@
-import { useState } from 'react'
 import AppName from './components/AppName'
 import AddTodo from './components/AddTodo'
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <center className = 'todo-container'>
