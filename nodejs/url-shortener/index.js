3cdd67cb87d36571f8d9ec078001bb000811ec49
arnob <farhanasfar21@gmail.com>
2024-11-24 23:07:36 +0600

    static route

--- nodejs/url-shortener/index.js
+++ nodejs/url-shortener/index.js
@@ -3,23 +3,28 @@ const urlRoute = require('./routes/url');
 const { connectMongoDB } = require('./connection');
 const URL = require('./models/url');
 const path = require('path');
+const staticRoute = require('./routes/staticRouter');
 
 const app = express();
 const PORT = 8000;
-
-app.set('view engine', 'ejs');
-app.set('views', path.resolve("./views"));
 
 connectMongoDB('mongodb://localhost:27017/short-url')
 .then(()=> {
     console.log("MongoDB connected");
 })
 
+app.set('view engine', 'ejs');
+app.set('views', path.resolve("./views"));
+
+app.use('/', staticRoute);
+
 app.use(express.json());
 
 app.get('/test', async (req, res)=>{
     const urls = await URL.find({});
-    return res.render('home');
+    return res.render('home', {
+        urls: urls,
+    });
 })
 
 app.use('/url', urlRoute);
