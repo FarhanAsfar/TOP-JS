82f1d71d5750ead58abdf0780b6a27b5eba3958a
arnob <farhanasfar21@gmail.com>
2024-11-24 22:59:03 +0600

    render ejs file

--- nodejs/url-shortener/index.js
+++ nodejs/url-shortener/index.js
@@ -2,9 +2,13 @@ const express = require('express');
 const urlRoute = require('./routes/url');
 const { connectMongoDB } = require('./connection');
 const URL = require('./models/url');
+const path = require('path');
 
 const app = express();
 const PORT = 8000;
+
+app.set('view engine', 'ejs');
+app.set('views', path.resolve("./views"));
 
 connectMongoDB('mongodb://localhost:27017/short-url')
 .then(()=> {
@@ -12,6 +16,11 @@ connectMongoDB('mongodb://localhost:27017/short-url')
 })
 
 app.use(express.json());
+
+app.get('/test', async (req, res)=>{
+    const urls = await URL.find({});
+    return res.render('home');
+})
 
 app.use('/url', urlRoute);
 
