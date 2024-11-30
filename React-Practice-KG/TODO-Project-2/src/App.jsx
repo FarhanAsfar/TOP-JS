bcf37a54d24ed9ccc3ba34dcd0b08ecfc439f645
arnob <farhanasfar21@gmail.com>
2024-11-30 23:45:26 +0600

    todo app -2

--- React-Practice-KG/TODO-Project-2/src/App.jsx
+++ React-Practice-KG/TODO-Project-2/src/App.jsx
@@ -1,14 +1,20 @@
 import AppName from './components/AppName'
 import AddTodo from './components/AddTodo'
 import "./App.css";
+import TodoItem from './components/TodoItem';
 
 function App() {
 
   return (
-    <center className = 'todo-container'>
-      <AppName />
-      <AddTodo />
-    </center>
+    <div>
+      <center className = 'todo-container'>
+        <AppName />
+        <AddTodo />
+      </center>
+      <div className='todo-container'>
+          <TodoItem todoName='something' todoDate='29/11/24'></TodoItem>
+      </div>
+    </div>
   )
 }
 
