ba53481dc9633aaac893ede05453b3d5771b82c7
arnob <farhanasfar21@gmail.com>
2024-12-01 23:52:37 +0600

    add css

--- React-Practice-KG/TODO-Project-2/src/components/AppName.jsx
+++ React-Practice-KG/TODO-Project-2/src/components/AppName.jsx
@@ -1,7 +1,7 @@
 import styles from "./AppName.module.css";
 
 function AppName(){
-    return <h1>Todo App (React)</h1>
+    return <h1 className={styles.todoHeading}>Todo App (React)</h1>
 }
 
 export default AppName;
