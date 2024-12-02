9fcd758e59e57b65eac122af8bf950539ba499a7
arnob <farhanasfar21@gmail.com>
2024-12-02 17:07:25 +0600

    generate session id for user

--- nodejs/url-shortener/controllers/user.js
+++ nodejs/url-shortener/controllers/user.js
@@ -1,5 +1,6 @@
 const User = require("../models/user");
 const {v4: uuidv4} = require("uuid");
+const { setUser } = require('../service/auth');
 
 async function handleUserSignup(req, res){
     const { name, email, password } = req.body;
