d8eefeb46b03985885b3ed3fb369f33f4316378a
arnob <farhanasfar21@gmail.com>
2024-12-10 02:32:22 +0600

    add bootstrap

--- blog-app/index.js
+++ blog-app/index.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const PORT = 8000;
+
+app.set('view engine', 'ejs');
+app.set('views', path.resolve("./views"));
+
 
 
 app.listen(PORT, ()=>{
