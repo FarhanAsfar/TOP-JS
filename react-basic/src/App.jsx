2e03a3a2fce5160ab8d99475ca6962f00cf9088b
arnob <farhanasfar21@gmail.com>
2024-12-13 01:00:37 +0600

    changes

--- react-basic/src/App.jsx
+++ react-basic/src/App.jsx
@@ -1,35 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import List from './components/list'
-
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  
 
   return (
-    <div>
-      {/* component name must Start with an Uppercase letter */}
-      <CustomButton count={count} setCount={setCount}></CustomButton>
-      <div><List></List></div>
-    </div>
+    <>
+
+    </>
   )
 }
 
-//component
-function CustomButton(props){
-  
-  function onClickHandler(){
-    props.setCount(props.count+1);
-    //console.log(state.count); 
-  }
-  
-  return <button onClick={onClickHandler}>
-    Counter {props.count}
-  </button>
-}
 
 export default App
