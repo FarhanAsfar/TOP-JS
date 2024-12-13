200c5c32f38326286392d63b75493d766b2a9386
arnob <farhanasfar21@gmail.com>
2024-12-14 03:05:04 +0600

    Use 'navigate' inside useEffect

--- react-basic/src/components/PlanetsList.jsx
+++ react-basic/src/components/PlanetsList.jsx
@@ -1,11 +1,20 @@
+import { useEffect, useState } from "react";
 import "../components/PlanetsList.css";
 import { useNavigate } from "react-router-dom";
 
 function PlanetsList({ planets }) {
     const navigate = useNavigate();
+    const [currentPlanetId, setCurrentPlanetId] = useState(null);
 
+    useEffect(() => {
+        if(currentPlanetId){
+            navigate(`/details/${currentPlanetId}`);
+        }
+    },[currentPlanetId, navigate])
+    
     const handleCardClick = (planetId) => {
-        navigate(`/details/${planetId}`)
+        // navigate(`/details/${planetId}`);
+        setCurrentPlanetId(planetId);
     }
     
     return (
