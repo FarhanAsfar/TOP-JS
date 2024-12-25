499ecbe88d6c722011c5c337cd4949639afe6ae6
arnob <farhanasfar21@gmail.com>
2024-12-26 00:54:10 +0600

    install tailwind

---
+++ tailwind/tailwind-basic/src/App.jsx
@@ -0,0 +1,16 @@
+
+
+function App() {
+
+  return (
+    <>
+      <div style={{display: "flex", justifyContent:"space-between"}}>
+        <div style={{background:"red",  width:300}}>hello</div>
+        <div style={{background:"blue", width:300}}>hello</div>
+        <div style={{background:"grey", width:300}}>hello</div>
+      </div>
+    </>
+  )
+}
+
+export default App
