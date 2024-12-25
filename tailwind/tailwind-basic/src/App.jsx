b1d3f5bce72c9b3696540a22d23d1207f359b668
arnob <farhanasfar21@gmail.com>
2024-12-26 02:21:48 +0600

    add responsiveness

--- tailwind/tailwind-basic/src/App.jsx
+++ tailwind/tailwind-basic/src/App.jsx
@@ -4,10 +4,10 @@ function App() {
 
   return (
     <>
-      <div style={{display: "flex", justifyContent:"space-between"}}>
-        <div style={{background:"red",  width:300}}>hello</div>
-        <div style={{background:"blue", width:300}}>hello</div>
-        <div style={{background:"grey", width:300}}>hello</div>
+      <div className="grid grid-cols-3 gap-3">
+        <div className="col-span-3 bg-cyan-800 sm:col-span-1">earth</div>
+        <div className="col-span-3 bg-green-400 sm:col-span-1">venuus</div>
+        <div className="col-span-3 bg-blue-300 sm:col-span-1">mercury</div>
       </div>
     </>
   )
