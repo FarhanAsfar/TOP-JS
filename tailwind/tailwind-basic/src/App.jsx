cd5a4fe909428e3168a0d57d6da71296c8d54517
arnob <farhanasfar21@gmail.com>
2024-12-27 02:03:01 +0600

    responsive grid

--- tailwind/tailwind-basic/src/App.jsx
+++ tailwind/tailwind-basic/src/App.jsx
@@ -4,10 +4,10 @@ function App() {
 
   return (
     <>
-      <div className="grid grid-cols-3 gap-3">
-        <div className="col-span-3 bg-cyan-800 sm:col-span-1">earth</div>
-        <div className="col-span-3 bg-green-400 sm:col-span-1">venuus</div>
-        <div className="col-span-3 bg-blue-300 sm:col-span-1">mercury</div>
+      <div className="grid grid-cols-1 sm:grid-cols-3 gap-3">
+        <div className="bg-cyan-800">earth</div>
+        <div className="bg-green-400">venuus</div>
+        <div className="bg-blue-300">mercury</div>
       </div>
     </>
   )
