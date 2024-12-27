2792c9c68538d02b4d108ded4a03a1cd9531012c
arnob <farhanasfar21@gmail.com>
2024-12-27 18:07:38 +0600

    change

--- tailwind/tailwind-basic/src/App.jsx
+++ tailwind/tailwind-basic/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
       <div className="grid grid-cols-1 sm:grid-cols-3 gap-3">
         <div className="bg-cyan-800">earth</div>
         <div className="bg-green-400">venuus</div>
-        <div className="bg-blue-300">mercury</div>
+        <div className="bg-blue-300">mercury</div> 
       </div>
     </>
   )
