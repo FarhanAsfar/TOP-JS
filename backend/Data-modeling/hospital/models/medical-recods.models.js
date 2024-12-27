34ced47539fe2e101d52b3fdce53f8e913325849
arnob <farhanasfar21@gmail.com>
2024-12-28 03:00:04 +0600

    medical-record shcema

--- backend/Data-modeling/hospital/models/medical-recods.models.js
+++ backend/Data-modeling/hospital/models/medical-recods.models.js
@@ -1,7 +1,22 @@
 const mongoose = require("mongoose");
 
 const medicalRecordSchema = new mongoose.Schema({
-
+    userName: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Patient",
+    },
+    hospitalName: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Hospital",
+    },
+    diagnosedWith: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Patient",
+    },
+    checkedBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Doctor",
+    },
 },{timestamps: true});
 
 
