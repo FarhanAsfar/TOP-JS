1e2995d0349c8198061443d790d65ca6a0eb6dec
arnob <farhanasfar21@gmail.com>
2024-12-28 02:39:04 +0600

    doctors schema

--- backend/Data-modeling/hospital/models/doctors.models.js
+++ backend/Data-modeling/hospital/models/doctors.models.js
@@ -1,7 +1,26 @@
 const mongoose = require("mongoose");
 
 const doctorSchema = new mongoose.Schema({
-
+    name: {
+        type: String,
+        required: true,
+    },
+    salary: {
+        type: String,
+    },
+    qualifications: {
+        type: String,
+        required: true,
+    },
+    experienceInYears: {
+        type: Number,
+    },
+    workingHospital: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Hospital",
+        },
+    ],
 },{timestamps: true});
 
 
