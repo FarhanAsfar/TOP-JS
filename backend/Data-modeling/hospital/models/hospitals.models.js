b26326c60a4f08b401770ee529dec8a05a52a06b
arnob <farhanasfar21@gmail.com>
2024-12-28 02:42:33 +0600

    hospital schema

--- backend/Data-modeling/hospital/models/hospitals.models.js
+++ backend/Data-modeling/hospital/models/hospitals.models.js
@@ -1,7 +1,25 @@
 const mongoose = require("mongoose");
 
 const hospitalSchema = new mongoose.Schema({
-
+    name: {
+        type: String,
+        required: true,
+    },
+    address: {
+        type: String,
+        required: true,
+    },
+    city: {
+        type: String,
+    },
+    pincode: {
+        type: String,
+    },
+    specializedIn: [
+        {
+            type: String,
+        },
+    ],
 },{timestamps: true});
 
 
