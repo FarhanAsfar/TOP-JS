0ef0b3f0fb318b6ac61d8f9a1b527ff2288ecb1f
arnob <farhanasfar21@gmail.com>
2025-01-02 19:43:12 +0600

    prototype chain

--- js/object_constructor/prototype.js
+++ js/object_constructor/prototype.js
@@ -10,6 +10,6 @@ const object2 = Object.create(object1);
 object2.department = 'CSE';
 
 
-console.log(`${object2.name} ${object2.age}`); 
+console.log(`${object2.name} ${object2.age}`);//object2 will also have access of the object1's properties as its inherits from object1. 
 
 console.log(object1.department);
