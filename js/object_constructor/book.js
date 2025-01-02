3e1913a974e96aaf7bcb315ca151e5b8c15a092e
arnob <farhanasfar21@gmail.com>
2025-01-02 18:55:52 +0600

    JS Prototype

--- js/object_constructor/book.js
+++ js/object_constructor/book.js
@@ -11,6 +11,11 @@ function Book(title, author, pages, read){
 
 }
 
+Book.prototype.rating = '4.4';
+
 const book1 = new Book('The Hobbit', 'J.R.R', '249', 'read');
 
 console.log(book1.info());
+console.log(book1.rating);
+
+// console.log(Object.getPrototypeOf(book1));
