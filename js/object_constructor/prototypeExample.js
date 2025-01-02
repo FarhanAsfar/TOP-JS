8a806ef8435133491fb62f9ff0ddc1c48b14f562
arnob <farhanasfar21@gmail.com>
2025-01-02 20:32:49 +0600

    Comparison: with and without using 'Protoype'

---
+++ js/object_constructor/prototypeExample.js
@@ -0,0 +1,24 @@
+//Without using prototype
+function Student(name, department){
+    this.name = name;
+    this.department = department;
+
+    // this.info = function (){
+    //     const info = `${this.name} from ${this.department} department`;
+
+    //     return info;
+    // }
+}
+
+//Using prototype
+Student.prototype.info = function(){
+    const info = (`${this.name} from ${this.department} department`);
+
+    return info;
+}
+
+const student1 = new Student('farhan', 'cse');
+const student2 = new Student('alice', 'eee');
+
+console.log(student1.info());
+console.log(student2.info());
