3e2078f5093efc683bb434684d2fd31c7f9d69fc
arnob <farhanasfar21@gmail.com>
2025-01-08 02:13:25 +0600

    blog post area + multer for file upload

--- blog-app/routes/blog.js
+++ blog-app/routes/blog.js
@@ -1,10 +1,29 @@
 const { Router } = require('express');
+const multer = require('multer');
 const router = Router();
+const path = require('path')
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, path.resolve(`./public/upload/`))
+    },
+    filename: function (req, file, cb) {
+      const fileName = `${Date.now()}-${file.originalName}`
+      cb(null, fileName);
+    }
+  })
+  
+  const upload = multer({ storage: storage })
 
 router.get('/add-new', (req,res)=>{
     return res.render('addBlog', {
         user: req.user,
     })
+});
+
+
+router.post('/', upload.single('coverImage'), (req,res)=>{
+    return res.redirect('/');
 })
 
 
