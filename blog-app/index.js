1e238e513b1a6058aa1e80b243b09bdd983f86b9
farhan <farhanasfar21@gmail.com>
2025-01-07 14:02:21 +0600

    add signup page

--- blog-app/index.js
+++ blog-app/index.js
@@ -4,12 +4,16 @@ const path = require('path');
 const app = express();
 const PORT = 8000;
 
+const userRouter = require('./routes/user');
+
 app.set('view engine', 'ejs');
 app.set('views', path.resolve("./views"));
 
 app.get('/', (req, res) => {
     res.render('home');
-})
+});
+
+app.use('/user', userRouter);
 
 app.listen(PORT, ()=>{
     console.log(`Server is running on Port: ${PORT}`);
