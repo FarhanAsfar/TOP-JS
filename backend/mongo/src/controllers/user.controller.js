3e7be536866a8945a3c1034e1bebe2b8db14e3d9
arnob <farhanasfar21@gmail.com>
2025-02-03 00:02:05 +0600

    input validation

--- backend/mongo/src/controllers/user.controller.js
+++ backend/mongo/src/controllers/user.controller.js
@@ -1,9 +1,24 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js"
+import { User } from "../models/user.model.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-    res.status(200).json({
-        message: "OK"
-    })
+    const {username, fullName, email, password} = req.body;
+
+    if([fullName, email, username, password].some((field) => field?.trim() === "")){
+        throw new ApiError(400, "All fields are required")
+    }
+
+    const exitsedUser = User.findOne({
+        $or: [
+                {username},
+                {email}
+            ]
+    });
+
+    if(exitsedUser){
+        throw new ApiError(409, "Username or Email already exists")
+    }
 });
 
 
