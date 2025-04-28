af949ec50f138d02ea2e33240522f34a5a5d83ac
arnob <farhanasfar21@gmail.com>
2025-04-29 03:24:32 +0600

    fix

--- backend/mongo/src/models/video.model.js
+++ backend/mongo/src/models/video.model.js
@@ -40,4 +40,4 @@ const videoSchema = new Schema({
 
 videoSchema.plugin(mongooseAggregatePaginate);
 
-export const Video = monggose.model("Video", videoSchema);
+export const Video = mongoose.model("Video", videoSchema);
