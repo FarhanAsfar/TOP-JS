e97265b9a12e5b9202d43ba2b0ab7edce5697bbc
arnob <farhanasfar21@gmail.com>
2025-07-14 19:46:55 +0600

    Add comment route

---
+++ backend/mongo/src/routes/comment.routes.js
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import { verifyJWT } from "../middlewares/auth.middleware";
+
+const router = Router();
+
+
+router.route("/add-comment/:id").post(verifyJWT, addComment);
+router.route("/delete-comment/:id").delete(verifyJWT, deleteComment);
+
+router.route("/edit-comment/:id").put(verifyJWT, editComment);
+
+export default router;
