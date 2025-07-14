0476566aca2fb92d8aeea418ba35f6e0d502d8eb
arnob <farhanasfar21@gmail.com>
2025-07-14 19:58:33 +0600

    Add video route

---
+++ backend/mongo/src/routes/video.routes.js
@@ -0,0 +1,27 @@
+import { Router } from "express";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+
+const router = Router();
+
+
+router.route("/publish-video").post(
+    upload.fields([
+        {
+            name: "videoFile",
+            maxCount: 1,
+        },
+        {
+            name: "thumbnail",
+            maxCount: 1,
+        }
+    ]),
+    verifyJWT, publishVideo);
+
+router.route("/all-videos").get(getAllVideos);
+
+router.route("/get-video/:id").get(verifyJWT, getVideoById);
+
+router.route("/delete-video/:id").delete(verifyJWT, deleteVideo);
+
+
+export default router;
